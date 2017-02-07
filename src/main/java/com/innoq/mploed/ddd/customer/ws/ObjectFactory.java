ef96dc2be0c1bcaf052614fe4cb0a302940bcb0e
mploed <michael.ploed@gmail.com>
2017-02-07 09:05:45 +0100

    cf adjustments

--- src/main/java/com/innoq/mploed/ddd/customer/ws/ObjectFactory.java
+++ src/main/java/com/innoq/mploed/ddd/customer/ws/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.7 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2017.02.07 um 08:12:31 AM CET 
+// Generiert: 2017.02.07 um 09:02:45 AM CET 
 //
 
 
