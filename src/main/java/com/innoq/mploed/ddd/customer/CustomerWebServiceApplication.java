98830bfce6f571c273c0a2bf948cbdfbe615ebd0
mploed <michael.ploed@gmail.com>
2017-03-24 08:35:51 +0100

    ws adjustments

--- src/main/java/com/innoq/mploed/ddd/customer/CustomerWebServiceApplication.java
+++ src/main/java/com/innoq/mploed/ddd/customer/CustomerWebServiceApplication.java
@@ -3,8 +3,10 @@ package com.innoq.mploed.ddd.customer;
 import com.innoq.mploed.ddd.customer.ws.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CustomerWebServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerWebServiceApplication.class);
